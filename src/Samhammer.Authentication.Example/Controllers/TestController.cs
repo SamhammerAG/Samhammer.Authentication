0f47b733f8380c2d7a6f91a30d5c36e4f10a9796
Roland Schmitt <rschmitt@samhammer.de>
2020-10-21 18:33:46 +0200

    add example

---
+++ src/Samhammer.Authentication.Example/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Samhammer.Authentication.Example.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
