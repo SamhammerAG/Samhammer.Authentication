2ae85f097196f02675ed79679cb96675421ec96f
Roland Schmitt <rschmitt@samhammer.de>
2020-10-23 17:12:46 +0200

    update example for guest authentication

--- src/Samhammer.Authentication.Example/Controllers/TestController.cs
+++ src/Samhammer.Authentication.Example/Controllers/TestController.cs
@@ -1,17 +1,58 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Samhammer.Authentication.Api.Guest;
 
 namespace Samhammer.Authentication.Example.Controllers
 {
     [ApiController]
-    [Authorize]
     [Route("api/[controller]")]
     public class TestController : ControllerBase
     {
-        [HttpGet]
+        [Authorize(AuthenticationSchemes = GuestAuthenticationDefaults.AuthenticationScheme + ", " + JwtBearerDefaults.AuthenticationScheme, Roles = "default")]
+        [HttpGet("both")]
         public string Get()
         {
-            return "Hello World";
+            return $"Welcome {User.Identity.Name}!";
+        }
+
+        [Authorize(Roles = "default")]
+        [HttpGet("unspecific")]
+        public string GetUnspecified()
+        {
+            return $"Welcome {User.Identity.Name}!";
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "default")]
+        [HttpGet("keyCloakOnly")]
+        public string GetKeyCloak()
+        {
+            return $"Welcome {User.Identity.Name}!";
+        }
+
+        [Authorize(AuthenticationSchemes = GuestAuthenticationDefaults.AuthenticationScheme, Roles = "default")]
+        [HttpGet("userSessionOnly")]
+        public string GetUserSession()
+        {
+            return $"Welcome {User.Identity.Name}!";
+        }
+
+
+        [Authorize(AuthenticationSchemes = GuestAuthenticationDefaults.AuthenticationScheme + ", " + JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("userInfo")]
+        public ActionResult<object> GetUserInfo()
+        {
+            return new
+            {
+                loginName = User.Identity.Name,
+                isGuest = User.IsGuest(),
+                guestKey = User.GetGuestKey(),
+                firstName = User.FindFirst(ClaimTypes.GivenName)?.Value,
+                roles = string.Join(", ", User.FindAll(ClaimTypes.Role).Select(c => c.Value)),
+                lastName = User.FindFirst(ClaimTypes.Surname)?.Value
+            };
         }
     }
 }
