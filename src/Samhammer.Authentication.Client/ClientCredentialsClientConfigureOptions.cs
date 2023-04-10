a8decdd5e81d19ec31ecccbf9bb208f2fd7431c5
datlam162 <62109015+datlam162@users.noreply.github.com>
2023-04-10 08:34:57 +0700

    MET-1892: Update Authentication client library to replace IdentityModel by Duende

---
+++ src/Samhammer.Authentication.Client/ClientCredentialsClientConfigureOptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Duende.AccessTokenManagement;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Samhammer.Authentication.Client;
+
+public class ClientCredentialsClientConfigureOptions<TOptions> : IConfigureNamedOptions<ClientCredentialsClient>
+    where TOptions : class
+{
+    private readonly IServiceProvider serviceProvider;
+    private readonly Action<ClientCredentialsClient, TOptions> setupAction;
+    private readonly string clientName;
+
+    public ClientCredentialsClientConfigureOptions(
+        string clientName,
+        IServiceProvider serviceProvider,
+        Action<ClientCredentialsClient, TOptions> setupAction)
+    {
+        this.serviceProvider = serviceProvider;
+        this.setupAction = setupAction;
+        this.clientName = clientName;
+    }
+
+    public void Configure(ClientCredentialsClient options)
+    {
+        var apiAuthOptionsMonitor = serviceProvider.GetRequiredService<IOptionsMonitor<TOptions>>();
+        apiAuthOptionsMonitor.OnChange(apiAuthOptions =>
+        {
+            setupAction(options, apiAuthOptions);
+        });
+
+        setupAction(options, apiAuthOptionsMonitor.CurrentValue);
+    }
+
+    public void Configure(string name, ClientCredentialsClient options)
+    {
+        if (clientName.Equals(name))
+        {
+            Configure(options);
+        }
+    }
+}
---
+++ src/Samhammer.Authentication.Client/ClientCredentialsConfigureExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Duende.AccessTokenManagement;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Samhammer.Authentication.Client;
+
+public static class ClientCredentialsConfigureExtensions
+{
+    public static void AddClientCredentialsOptions<TOptions>(
+        this IServiceCollection services,
+        string clientName,
+        Action<ClientCredentialsClient, TOptions> configureOptions)
+        where TOptions : class
+    {
+
+        services.AddSingleton<IConfigureOptions<ClientCredentialsClient>>(provider =>
+            new ClientCredentialsClientConfigureOptions<TOptions>(clientName, provider, configureOptions));
+    }
+}
