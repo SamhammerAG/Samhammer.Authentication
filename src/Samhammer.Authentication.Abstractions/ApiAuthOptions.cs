7e8800db1ebaa0c28a4bbc62aa14b99cf41c24b2
Roland Schmitt <rschmitt@samhammer.de>
2023-04-14 15:52:06 +0200

    SSP-4272 add options for client authentication to ApiAuthOptions can be used for httpClient by Samhammer.Authentication.Client

--- src/Samhammer.Authentication.Abstractions/ApiAuthOptions.cs
+++ src/Samhammer.Authentication.Abstractions/ApiAuthOptions.cs
@@ -6,6 +6,12 @@
 
         public string ClientId { get; set; }
 
+        public string ClientSecret { get; set; }
+
+        public string AccessTokenUrl { get; set; }
+
         public string NameClaim { get; set; } = "preferred_username";
+
+        public const string DefaultClientName = "default";
     }
 }
